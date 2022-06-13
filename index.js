8f7c3908f290ee6ae533e717cd47c8040340dced
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-13 15:32:52 +0300

    commit tree 3a05d6edac08890e98ea3d0ab8b85a1781acb8ae

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(arr) {
+    let newArr = [];
+    for (let i = 0; i < 2; i++) {
+      newArr.push(arr[i]);
+    }
+    return newArr;
+  }
+  
+  function returnLastTwoDrivers(arr) {
+    let newArr = [];
+    for (let i = arr.length - 2; i < arr.length; i++) {
+      newArr.push(arr[i]);
+    }
+    return newArr;
+  }
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  function createFareMultiplier(fare) {
+    return () => {
+      return fare * fare;
+    }
+  }
+  
+  function fareDoubler(fare) {
+    return fare * 2;
+  }
+  
+  function fareTripler(fare) {
+    return fare * 3;
+  }
+  
+  function selectDifferentDrivers(arrayOfDrivers, f) {
+    return f(arrayOfDrivers);
+  }
